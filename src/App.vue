43beca91f3379627994aedfc205e568d4a924b37
lzw <863834859@qq.com>
2024-03-19 23:11:42 +0800

    feat: 初始化应用

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="openUserGuide">新手指引</button>
+    <br>
+    <button id="btn1">第一步</button>
+    <br>
+    <button id="btn2">第二步</button>
+    <br>
+    <button id="btn3">第三步</button>
+    <br>
+    <button id="btn4">第四步</button>
+    <tour-helper ref="tour" :steps="steps"></tour-helper>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import tourHelper from './components/tour-helper.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    tourHelper
+  },
+  data() {
+    return {
+      steps: [{
+        title: 'Creating a Shepherd Tour',
+        text: 'Creating a Shepherd tour is easy. too! Just create a "Tour" instance, and add as many steps as you want.',
+        attachTo: {
+          element: '#btn1',
+          on: 'bottom'
+        },
+        buttons: [
+          {
+            action() {
+              return this.back();
+            },
+            classes: 'shepherd-button-secondary',
+            text: 'Back'
+          },
+          {
+            action() {
+              return this.next();
+            },
+            text: 'Next'
+          }
+        ],
+        id: 'btn1'
+      }]
+    }
+  },
+  methods: {
+    openUserGuide() {
+      this.$refs.tour.start()
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/tour-helper.vue
@@ -0,0 +1,50 @@
+
+<template>
+  <div style="display: none;"></div>
+</template>
+<script>
+import Shepherd from 'shepherd.js'
+export default {
+  name: 'tour-helper',
+  props: {
+    steps: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      tour: null,
+    }
+  },
+  created() {
+    this.tour = new Shepherd.Tour({
+      // 是否显示黑色遮罩层
+      useModalOverlay: true,
+      // 键盘按钮控制步骤
+      keyboardNavigation: true,
+      defaultStepOptions: {
+        // 显示关闭按钮
+        cancelIcon: {
+          enabled: true
+        },
+        classes: 'class-1 class-2',
+        scrollTo: { behavior: 'smooth', block: 'center' },
+        // 高亮元素四周要填充的空白像素
+        modalOverlayOpeningPadding: 4,
+        // 空白像素的圆角
+        modalOverlayOpeningRadius: 4,
+      }
+    });
+  },
+  methods: {
+    start() {
+      this.tour.addSteps(this.steps);
+      this.tour.start();
+    }
+  }
+}
+</script>
+</style>
